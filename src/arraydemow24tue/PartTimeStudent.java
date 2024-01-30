e5151b5b217deb90b9fe73a71d21351d9e0a3303
Ronak Sheth <ronak.sheth@sheridancollege.ca>
2024-01-30 12:41:06 -0500

    added class

---
+++ src/arraydemow24tue/PartTimeStudent.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package arraydemow24tue;
+
+/**
+ *
+ * @author Ronak
+ */
+public class PartTimeStudent extends Student {
+    private int numOfCourses;
+
+    public PartTimeStudent(int numOfCourses, int sid, String sname) {
+        super(sid, sname);
+        this.numOfCourses = numOfCourses;
+    }
+
+    
+    
+    
+    public int getNumOfCourses() {
+        return numOfCourses;
+    }
+
+    public void setNumOfCourses(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+    
+    
+}
